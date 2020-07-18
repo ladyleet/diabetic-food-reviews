6233648364216ef3db87890c91187d03a7720aad
Tracy Lee <tracy@modern-web.org>
2020-07-18 16:55:15 -0400

    nav-bar up

---
+++ src/components/MainNavigation.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card flat height="200px" tile>
+    <v-toolbar color="green" class="white--text" dense>
+      <v-toolbar-title>Diabetic Food Reviews</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "MainNavigation"
+});
+</script>
